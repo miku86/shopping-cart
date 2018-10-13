56d878ab49ec2f4ff6f5f5ad696c09c19e05db31
miku86 <miku86coding@gmail.com>
2018-10-13 11:14:40 +0200

    add clickhandler

--- src/components/PromoCode.js
+++ src/components/PromoCode.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import { Card, Button, InputGroup, FormControl } from 'react-bootstrap';
 
 class PromoCode extends Component {
+  state = {
+    promoCodeIsShown: false,
+  };
+
+  handleClick = () => {
+    this.setState({
+      promoCodeIsShown: !this.state.promoCodeIsShown,
+    });
+  };
+
   render() {
     const text = {
       title: 'Promo Code',
@@ -11,26 +21,41 @@ class PromoCode extends Component {
       buttonSubmit: ' Add Promo Code',
     };
 
-    return (
-      <Card style={{ width: '18rem' }}>
-        <Button variant="dark">{text.buttonShow}</Button>
-        <Card.Body>
-          <Card>
-            <Card.Body>
-              <Card.Subtitle className="mb-2 text-muted">
-                {text.title}
-              </Card.Subtitle>
-              <InputGroup className="mb-3">
-                <FormControl placeholder={text.input} aria-label={text.input} />
-              </InputGroup>
-              <Button variant="primary" size="sm">
-                {text.buttonSubmit}
-              </Button>
-            </Card.Body>
-          </Card>
-        </Card.Body>
-      </Card>
-    );
+    if (this.state.promoCodeIsShown) {
+      return (
+        <Card style={{ width: '18rem' }}>
+          <Button variant="dark" onClick={this.handleClick}>
+            {text.buttonHide}
+          </Button>
+          <Card.Body>
+            <Card>
+              <Card.Body>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {text.title}
+                </Card.Subtitle>
+                <InputGroup className="mb-3">
+                  <FormControl
+                    placeholder={text.input}
+                    aria-label={text.input}
+                  />
+                </InputGroup>
+                <Button variant="primary" size="sm">
+                  {text.buttonSubmit}
+                </Button>
+              </Card.Body>
+            </Card>
+          </Card.Body>
+        </Card>
+      );
+    } else {
+      return (
+        <Card style={{ width: '18rem' }}>
+          <Button variant="dark" onClick={this.handleClick}>
+            {text.buttonShow}
+          </Button>
+        </Card>
+      );
+    }
   }
 }
 
