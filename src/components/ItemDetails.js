3c32726a0eb276337b9e690046ec6ac1c6235255
miku86 <miku86coding@gmail.com>
2018-10-13 10:55:07 +0200

    remove unused style

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -21,7 +21,7 @@ class ItemDetails extends Component {
         <Button variant="dark">Show Items</Button>
         {items.map((item) => {
           return (
-            <Card.Body style={{}}>
+            <Card.Body>
               <Card>
                 <Card.Body>
                   <Card.Subtitle className="mb-2 text-muted">
