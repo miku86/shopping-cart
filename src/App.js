8415bf38b69a6f7bc78c043783da2fa647b83a95
miku86 <miku86coding@gmail.com>
2018-10-13 10:40:39 +0200

    add itemDetails component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import OrderOverview from './components/OrderOverview';
+import ItemDetails from './components/ItemDetails';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <OrderOverview />
+        <ItemDetails />
       </div>
     );
   }
---
+++ src/components/ItemDetails.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+class ItemDetails extends Component {
+  render() {
+    const items = [
+      {
+        title: '15-inch MacBook Pro',
+        description:
+          'Apple MacBook Pro 15.4, Core i9-8950HK, 32GB RAM, 4TB SSD, Radeon Pro 560X',
+      },
+      {
+        title: 'Apple iPhone XS Max 512GB gold',
+        description:
+          '6.5 inches, 1242 x 2688 pixels, Apple A12 Bionic Hexa-core, 512 GB Internal Storage, 4 GB RAM',
+      },
+    ];
+
+    return (
+      <Card style={{ width: '18rem' }}>
+        <Button variant="dark">Show Items</Button>
+        {items.map((item) => {
+          return (
+            <Card.Body style={{}}>
+              <Card>
+                <Card.Body>
+                  <Card.Subtitle className="mb-2 text-muted">
+                    {item.title}
+                  </Card.Subtitle>
+                  <Card.Text>{item.description}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Card.Body>
+          );
+        })}
+      </Card>
+    );
+  }
+}
+
+export default ItemDetails;
